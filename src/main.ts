842369165ce6c3fc1e85abbc7a52e94d55dbfdba
jhtong <me@joeltong.org>
2020-08-02 20:16:32 +0800

    removed redundant dependencies

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import cors from 'cors';
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import helmet from 'helmet';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
 
 import { logger, loggingMiddleware } from './utils/logger';
 
@@ -51,42 +49,6 @@ app.use((err, req, res, next): void => {
 
   next(err);
 });
-
-// Swagger set up
-const options = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Time to document that Express API you built',
-      version: '1.0.0',
-      description:
-        'A test project to understand how easy it is to document and Express API',
-      license: {
-        name: 'MIT',
-        url: 'https://choosealicense.com/licenses/mit/',
-      },
-      contact: {
-        name: 'Joel Tong',
-        url: 'https://devnull.io',
-        email: 'me@joeltong.org',
-      },
-    },
-    servers: [
-      {
-        url: `http://localhost:${PORT}`,
-      },
-    ],
-  },
-  apis: ['./controller/studentController.js'],
-};
-const specs = swaggerJsdoc(options);
-app.use('/docs', swaggerUi.serve);
-app.get(
-  '/docs',
-  swaggerUi.setup(specs, {
-    explorer: true,
-  }),
-);
 
 // start app
 const httpServer = http.createServer(app);
